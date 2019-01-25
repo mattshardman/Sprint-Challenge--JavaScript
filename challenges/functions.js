cf6ca71ab2022f01a2edd9dbfe152ab7e7d84019
mattshardman <mattshardman@gmail.com>
2019-01-25 08:46:24 +0000

    Funcitons step1: create HOF called consume

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,13 +6,16 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(arg1, arg2, cb) {
+  return cb(arg1, arg2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
