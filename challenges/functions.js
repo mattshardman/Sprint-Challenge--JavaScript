0a0c43f97d39203b0d112dc4c5cffb851c58c737
mattshardman <mattshardman@gmail.com>
2019-01-25 08:49:25 +0000

    Funcitons step 2: create add multiply and greeting functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,8 +6,8 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-function consume(arg1, arg2, cb) {
-  return cb(arg1, arg2);
+function consume(a, b, cb) {
+  return cb(a, b);
 }
 
 /* Step 2: Create several functions to callback with consume();
@@ -15,7 +15,17 @@ function consume(arg1, arg2, cb) {
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(a, b) {
+  return a + b;
+}
 
+function multiply(a, b) {
+  return a * b;
+}
+
+function greeting(first, last) {
+  return `Hello ${first} ${last}, nice to meet you!`
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
