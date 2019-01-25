5d843914084c0daface943c521e48b39d12830b7
mattshardman <mattshardman@gmail.com>
2019-01-25 08:31:10 +0000

    Arrays request 3: create new array of universities containing 'Uni'

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -88,7 +88,7 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const uni = graduates.filter(graduate => graduate.university.includes("Uni"));
 console.log(uni);
 
 
