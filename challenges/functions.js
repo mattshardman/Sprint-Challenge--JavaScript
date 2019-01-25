acd3805678e2397807e169336aa0c93e7c8218ed
mattshardman <mattshardman@gmail.com>
2019-01-25 08:51:41 +0000

    Functions step 3: test consume function

--- challenges/functions.js
+++ challenges/functions.js
@@ -16,22 +16,28 @@ function consume(a, b, cb) {
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 function add(a, b) {
-  return a + b;
+  const result = a + b;
+  console.log(result)
+  return result;
 }
 
 function multiply(a, b) {
-  return a * b;
+  const result = a * b;
+  console.log(result);
+  return result;
 }
 
 function greeting(first, last) {
-  return `Hello ${first} ${last}, nice to meet you!`
+  const result = `Hello ${first} ${last}, nice to meet you!`;
+  console.log(result);
+  return result;
 }
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
