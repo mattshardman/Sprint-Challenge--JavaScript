efe730a895cde07006c86319ad42f17b44318ba1
mattshardman <mattshardman@gmail.com>
2019-01-25 08:37:51 +0000

    Arrays animals request 2: array of animals with a population of less that 5

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -129,10 +129,10 @@ console.log(lowerCase);
 
 /* Request 3: .filter() 
 
-The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
+The zoos are concerned about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+const largerPopulation = zooAnimals.filter(animal => animal.population < 5);
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
